e96433e5259e4ccbaf497597cf310d4a77a4a155
root <root@46-typo3-dev.ip-5-39-66.eu>
2015-03-05 12:56:44 +0100

    First commit

---
+++ src/Handler.php
@@ -0,0 +1,90 @@
+<?php
+namespace GitHubWebhook;
+
+class Handler
+{
+    private $secret;
+    private $remote;
+    private $gitDir;
+    private $data;
+    private $event;
+    private $delivery;
+    private $gitOutput;
+
+    public function __construct($secret, $gitDir = null, $remote = null)
+    {
+        $this->secret = $secret;
+        $this->remote = $remote;
+        $this->gitDir = $gitDir;
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function getDelivery()
+    {
+        return $this->delivery;
+    }
+
+    public function getEvent()
+    {
+        return $this->event;
+    }
+
+    public function getGitDir()
+    {
+        return $this->gitDir;
+    }
+
+    public function getGitOutput()
+    {
+        return $this->gitOutput;
+    }
+
+    public function getRemote()
+    {
+        return $this->remote;
+    }
+
+    public function getSecret()
+    {
+        return $this->secret;
+    }
+
+    public function handle()
+    {
+        if (!$this->validate()) {
+            return false;
+        }
+
+        chdir($this->gitDir);
+        exec(" git pull -f {$this->remote}", $this->gitOutput);
+
+        return true;
+    }
+
+    public function validate()
+    {
+        $headers = apache_request_headers();
+        $payload = file_get_contents('php://input');
+
+        if (!$this->validateSignature($headers['X-Hub-Signature'], $payload)) {
+            return false;
+        }
+
+        $this->data = json_decode($payload,true);
+	$this->gitDir = $this->data['repository']['name'];
+        $this->event = $headers['X-GitHub-Event'];
+        $this->delivery = $headers['X-GitHub-Delivery'];
+        return true;
+    }
+
+    protected function validateSignature($gitHubSignatureHeader, $payload)
+    {
+        list ($algo, $gitHubSignature) = explode("=", $gitHubSignatureHeader);
+        $payloadHash = hash_hmac($algo, $payload, $this->secret);
+        return ($payloadHash == $gitHubSignature);
+    }
+}
